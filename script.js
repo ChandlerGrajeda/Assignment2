c14308c49362ad2b4a1a10c3adf8b6bf51953120
chandler Grajeda <chandlerg25@gmail.com>
2025-04-07 20:11:04 -0500

    update

---
+++ script.js
@@ -0,0 +1,29 @@
+//Array
+
+let students = ["Cameron", "Jordan", "Renna"]
+
+let student = [
+  { name: "Cameron",
+    age: 25,
+    isEnrolled: true 
+  },
+  { name: "Jordan",
+      age: 29,
+      isEnrolled: true 
+  },
+  { name: "Renna",
+      age: 24,
+      isEnrolled: false 
+  },
+]
+
+console.log (students)
+console.log (student[0])
+console.log (student[1])
+console.log (student[2])
+console.log (student[0].name)
+console.log (student[1].age)
+console.log (student[2].isEnrolled)
+console.log (typeof(student[0].name))
+console.log (typeof(student[0].age))
+console.log (typeof(student[0].isEnrolled))
